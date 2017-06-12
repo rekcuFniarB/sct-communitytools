42257f49016c41c2aa5a9e636e2e111125edbcfa
BrainFucker <retratserif@gmail.com>
2017-06-13 01:18:12 +0300

    Fixing sphblog.urls

--- sphenecoll/sphene/sphblog/urls.py
+++ sphenecoll/sphene/sphblog/urls.py
@@ -1,12 +1,14 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import url
 
 from sphene.sphblog.feeds import LatestBlogPosts
+
+from sphene.sphblog import views as sphblog_views
 
 #feeds = {
 #    'latestposts': LatestBlogPosts,
 #}
 
-urlpatterns = patterns('',
+urlpatterns = [
         url(r'^feeds/latestposts/$', LatestBlogPosts(),
                             { 
                               'noGroup': True,
@@ -17,24 +19,24 @@ urlpatterns = patterns('',
                               'noGroup': True,
                               },
                             name = 'sphblog-feeds'),
-                       )
-urlpatterns += patterns('sphene.sphblog.views',
-                       url(r'^$', 'blogindex', name='sphblog_index'),
-                       url(r'^postthread/$', 'postthread', name = 'sphblog_postthread'),
-                       url(r'^tag/(?P<tag_name>\w+)/(?:page/(?P<page>\d+)/)?$', 'show_tag_posts', name = 'sphblog_show_tag_posts'),
+                       ]
+urlpatterns.extend(
+                       url(r'^$', sphblog_views.blogindex, name='sphblog_index'),
+                       url(r'^postthread/$', sphblog_views.postthread, name = 'sphblog_postthread'),
+                       url(r'^tag/(?P<tag_name>\w+)/(?:page/(?P<page>\d+)/)?$', sphblog_views.show_tag_posts, name = 'sphblog_show_tag_posts'),
 
                        # Indexes for year and month based archive
-                       url(r'^archive/(?P<year>\d{4})/$', 'blogindex', name='sphblog_archive_year'),
-                       url(r'^archive/(?P<year>\d{4})/(?P<month>\d{1,2})/$', 'blogindex', name='sphblog_archive_month'),
+                       url(r'^archive/(?P<year>\d{4})/$', sphblog_views.blogindex, name='sphblog_archive_year'),
+                       url(r'^archive/(?P<year>\d{4})/(?P<month>\d{1,2})/$', sphblog_views.blogindex, name='sphblog_archive_month'),
                        # Paged archives
-                       url(r'^archive/(?P<year>\d{4})/page/(?P<page>\d+)/$', 'blogindex', name='sphblog_archive_year_paged'),
-                       url(r'^archive/(?P<year>\d{4})/(?P<month>\d{1,2})/page/(?P<page>\d+)/$', 'blogindex', name='sphblog_archive_month_paged'),
+                       url(r'^archive/(?P<year>\d{4})/page/(?P<page>\d+)/$', sphblog_views.blogindex, name='sphblog_archive_year_paged'),
+                       url(r'^archive/(?P<year>\d{4})/(?P<month>\d{1,2})/page/(?P<page>\d+)/$', sphblog_views.blogindex, name='sphblog_archive_month_paged'),
 
-                       url(r'^page/(?P<page>\d+)/$', 'blogindex', name='sphblog_paged_index'),
+                       url(r'^page/(?P<page>\d+)/$', sphblog_views.blogindex, name='sphblog_paged_index'),
 
-                       (r'^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/(?P<slug>.*)/$', 'show_thread_redirect'),
-                       url(r'^(?P<category_id>\d+)/$', 'blogindex_redirect', name='sphblog_category_index'),
-                       url(r'^(?P<category_slug>[\w\-]+)/$', 'blogindex', name='sphblog_category_index_slug'),
-                       (r'^(?P<category_slug>[\w\-]+?)/(?P<slug>[\w\-]+)/$', 'show_thread'),
-                       )
+                       url(r'^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/(?P<slug>.*)/$', sphblog_views.show_thread_redirect),
+                       url(r'^(?P<category_id>\d+)/$', sphblog_views.blogindex_redirect, name='sphblog_category_index'),
+                       url(r'^(?P<category_slug>[\w\-]+)/$', sphblog_views.blogindex, name='sphblog_category_index_slug'),
+                       (r'^(?P<category_slug>[\w\-]+?)/(?P<slug>[\w\-]+)/$', sphblog_views.show_thread),
+                )
 
