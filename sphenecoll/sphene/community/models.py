13ee6df89398fa4bd4cd5a621e87520302fdeb15
BrainFucker <retratserif@gmail.com>
2017-06-13 01:04:33 +0300

    Fixing imports in models.py

--- sphenecoll/sphene/community/models.py
+++ sphenecoll/sphene/community/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes import fields as generic
 from django.utils.translation import ugettext as _, ugettext_lazy
 from django.db import connection
 from django.db.models.signals import post_save, post_delete
