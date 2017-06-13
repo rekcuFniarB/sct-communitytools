79410f6ed0844698af987a9d87d4b471286a9c2f
BrainFucker <retratserif@gmail.com>
2017-06-13 04:05:32 +0300

    Fixing sphwiki.urls

--- sphenecoll/sphene/sphwiki/urls.py
+++ sphenecoll/sphene/sphwiki/urls.py
@@ -1,24 +1,24 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import url
+from sphene.sphwiki import views
 
 
-
-urlpatterns = patterns('',
-                       (r'^$', 'django.views.generic.simple.redirect_to', {'url': 'show/Start/'}),
-                                              )
+urlpatterns = [
+                       url(r'^$', 'django.views.generic.simple.redirect_to', {'url': 'show/Start/'}),
+              ]
 
 snip = r'(?P<snipName>[\w/:\-.]+?)'
 
-urlpatterns += patterns('sphene.sphwiki.views',
-                        (r'^recentchanges/$', 'recentChanges'),
-                        (r'^show/'          + snip + r'/$', 'showSnip'),
-                        (r'^pdf/'           + snip + r'/$', 'generatePDF'),
-                        (r'^edit/'          + snip + r'/$', 'editSnip'),
-                        url(r'^editversion/'+ snip + r'/(?P<versionId>\d+)/$', 'editSnip', name = 'sphwiki_editversion'),
-                        (r'^history/'       + snip + r'/$', 'history'),
-                        (r'^diff/'          + snip + r'/(?P<changeId>\d+)/$', 'diff'),
-                        (r'^attachments/edit/'   + snip + r'/(?P<attachmentId>\d+)/$', 'attachmentEdit'),
-                        (r'^attachments/create/'   + snip + r'/$', 'attachmentCreate'),
-                        (r'^attachments/list/'   + snip + r'/$', 'attachment'),
+urlpatterns.extend([
+                        url(r'^recentchanges/$', views.recentChanges),
+                        url(r'^show/'          + snip + r'/$', views.showSnip),
+                        url(r'^pdf/'           + snip + r'/$', views.generatePDF),
+                        url(r'^edit/'          + snip + r'/$', views.editSnip),
+                        url(r'^editversion/'+ snip + r'/(?P<versionId>\d+)/$', views.editSnip, name = 'sphwiki_editversion'),
+                        url(r'^history/'       + snip + r'/$', views.history),
+                        url(r'^diff/'          + snip + r'/(?P<changeId>\d+)/$', views.diff),
+                        url(r'^attachments/edit/'   + snip + r'/(?P<attachmentId>\d+)/$', views.attachmentEdit),
+                        url(r'^attachments/create/'   + snip + r'/$', views.attachmentCreate),
+                        url(r'^attachments/list/'   + snip + r'/$', views.attachment),
 
-                        url(r'^tag/(?P<tag_name>\w+)/$', 'show_tag_snips', name = 'sphwiki_show_tag_snips'),
-                        )
+                        url(r'^tag/(?P<tag_name>\w+)/$', views.show_tag_snip, name = 'sphwiki_show_tag_snips'),
+                  ])
